461a8dfd4b9d3d9b67f727277b3cdee7a0b502d9
Adrian Estrada <51519261+aestrada2796@users.noreply.github.com>
2022-10-09 02:45:16 -0400

    Queries

--- src/Classes/Base.php
+++ src/Classes/Base.php
@@ -13,101 +13,43 @@ class Base
 
     /**
      * @autor Adrian Estrada
-     * @return string
-     */
-    protected function login(): string
-    {
-        $this->checkConfig();
-
-        $curl = curl_init();
-        $url = $this->routeBase . "/login";
-
-        $body = [
-            "query" => printf(Queries::$LOGIN, [config('mrconnect.api_user'), config('mrconnect.api_pass')])
-        ];
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => json_encode($body),
-            CURLOPT_HTTPHEADER => array(
-                'accept:  application/json',
-                'Content-Type: application/json',
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        return $response;
-    }
-
-    /**
-     * @autor Adrian Estrada
-     * @return void
-     */
-    private function checkConfig(): void
-    {
-        if (empty(config('mrconnect.api_route'))) {
-            response()->json([
-                "data" => [
-                    'error' => true,
-                    'message' => 'API path not found',
-                    'code' => 401
-                ]
-            ], 401);
-            return;
-        }
-
-        if (empty(config('mrconnect.api_user'))) {
-            response()->json([
-                "data" => [
-                    'error' => true,
-                    'message' => 'User not found',
-                    'code' => 401
-                ]
-            ], 401);
-            return;
-        }
-
-        if (empty(config('mrconnect.api_pass'))) {
-            response()->json([
-                "data" => [
-                    'error' => true,
-                    'message' => 'Password not found',
-                    'code' => 401
-                ]
-            ], 401);
-        }
-    }
-
-    /**
-     * @autor Adrian Estrada
      * @param $url
-     * @param array $query
-     * @return string|bool
+     * @param $query
      */
-    protected function sendRequest($url, array $query): string|bool
+    public function sendRequest($url, $query, $var = [])
     {
-        $this->checkConfig();
+        if (!session()->has('token')) {
+            $this->login();
+        }
+
+        $token = session('token');
+
+        if (empty($token)) {
+            return ["data" => [
+                'error' => true,
+                'message' => "Authentication Error",
+                'code' => 401
+            ]
+            ];
+        }
+
+        $error = $this->checkConfig();
+        if (!empty($error)) {
+            return $error;
+        }
 
         $curl = curl_init();
 
         $time = time();
-        $token = "123";
-        $signature = hash_hmac('sha256', json_encode($query), $time . $token);
         $body = [
-            "query" => $query
+            "query" => $query,
+            "variables" => json_encode($var)
         ];
 
+        $signature = hash_hmac('sha256', json_encode($body), $time . $token);
+
         curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
+            CURLOPT_URL => $this->routeBase . $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -128,6 +70,97 @@ class Base
         $response = curl_exec($curl);
 
         curl_close($curl);
-        return $response;
+        return json_decode($response, true);
+    }
+
+    /**
+     * @autor Adrian Estrada
+     */
+    public function login()
+    {
+        $error = $this->checkConfig();
+        if (!empty($error)) {
+            return $error;
+        }
+
+        $curl = curl_init();
+        $url = $this->routeBase . "/login";
+
+        $body = [
+            "query" => Queries::$LOGIN,
+            "variables" => [
+                "user" => config('mrconnect.api_user'),
+                "pass" => config('mrconnect.api_pass'),
+            ]
+        ];
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($body),
+            CURLOPT_HTTPHEADER => array(
+                'accept:  application/json',
+                'Content-Type: application/json',
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+
+        $login = json_decode($response, true);
+
+        if (isset($login["data"])) {
+            if (isset($login["data"]["login"])) {
+                if ($login["data"]["login"] != "Incorrect user or password") {
+                    session(["token" => $login["data"]["login"]]);
+                } else {
+                    session()->forget('token');
+                    return null;
+                }
+            }
+        }
+        return $login;
+    }
+
+    /**
+     * @autor Adrian Estrada
+     * @return array[]|null
+     */
+    private function checkConfig(): array|null
+    {
+        $error = [
+            "data" => [
+                'error' => true,
+                'message' => [
+
+                ],
+                'code' => 401
+            ]
+        ];
+        $response = false;
+
+        if (empty(config('mrconnect.api_route'))) {
+            $response = true;
+            $error["data"]["message"][] = 'API path not found';
+        }
+
+        if (empty(config('mrconnect.api_user'))) {
+            $response = true;
+            $error["data"]["message"][] = 'User not found';
+        }
+
+        if (empty(config('mrconnect.api_pass'))) {
+            $response = true;
+            $error["data"]["message"][] = 'Password not found';
+        }
+
+        return $response ? $error : null;
     }
 }
--- src/Classes/Queries.php
+++ src/Classes/Queries.php
@@ -4,7 +4,17 @@ namespace MrConnect\Classes;
 
 class Queries
 {
-    public static string $LOGIN = "mutation {
-            login(email:'%s',password:'%s')
-        }";
+    public static string $LOGIN = 'mutation Login($user: String!, $pass: String!) {
+            login(email: $user, password: $pass)
+        }';
+
+    public static string $TEST = 'query {
+        users(id: "5677f026-b5c6-474b-a927-6e90afd12d16"){
+            id,
+            name,
+            roles {
+                name
+            }
+        }
+    }';
 }
